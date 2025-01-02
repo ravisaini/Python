0c9ad57fd2faab48e772487bfa8d9f7c70518d16
ravisaini <rk.saini06@gmail.com>
2025-01-02 11:22:15 +0530

    area, and swap

---
+++ area.py
@@ -0,0 +1,4 @@
+length = int (input("Input length"))
+width =int ( input("Input width"))
+area = (length * width);
+print("Area Is", area);
---
+++ swap.py
@@ -0,0 +1,6 @@
+a= 20
+b=10
+b=a+b;
+a= b-a;
+b= b-a;
+print("a is ",a," b is " ,b)
